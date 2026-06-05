e391c5779aec1c823f0fa4f2eaf5d2e7cae8933c
xaxnx2 <aryayudistira10@gmail.com>
2026-06-06 01:40:27 +0700

    Fix: add pathRewrite false to all gateway routes

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -13,37 +13,44 @@ app.use(morgan('dev'))
 //Login Register
 app.use('/api/v1/registration', createProxyMiddleware({
     target: process.env.LOGIN_REG_URL || 'http://localhost:3000',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }))
 
 app.use('/api/v1/login', createProxyMiddleware({
     target: process.env.LOGIN_REG_URL || 'http://localhost:3000',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }))
 
 app.use('/api/v1/profile', createProxyMiddleware({
     target: process.env.LOGIN_REG_URL || 'http://localhost:3000',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }))
 
 // Information
 app.use('/api/v1/banner', createProxyMiddleware({
     target: process.env.INFORMATION_URL || 'http://localhost:3001',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }));
 app.use('/api/v1/services', createProxyMiddleware({
     target: process.env.INFORMATION_URL || 'http://localhost:3001',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }));
 
 //Payment
 app.use('/api/v1/balance', createProxyMiddleware({
     target: process.env.PAYMENT_URL || 'http://localhost:3002',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }));
 app.use('/api/v1/transaction', createProxyMiddleware({
     target: process.env.PAYMENT_URL || 'http://localhost:3002',
-    changeOrigin: true
+    changeOrigin: true,
+    pathRewrite: false
 }));
 
 app.listen(PORT, () => {
