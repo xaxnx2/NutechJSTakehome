d1d358d95a1d27c6bf503a867ffbd2f4c4cb93a4
xaxnx2 <aryayudistira10@gmail.com>
2026-06-06 02:57:05 +0700

    prependPath: true

--- api-gateway/server.js
+++ api-gateway/server.js
@@ -44,7 +44,7 @@ app.use('/api/v1/profile', createProxyMiddleware({
 app.use('/api/v1/banner', createProxyMiddleware({
     target: process.env.INFORMATION_URL || 'http://localhost:3001',
     changeOrigin: true,
-    pathRewrite: false
+    prependPath: true
 }));
 app.use('/api/v1/services', createProxyMiddleware({
     target: process.env.INFORMATION_URL || 'http://localhost:3001',
